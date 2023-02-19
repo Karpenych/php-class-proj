31240d55b3e9dad39046740b4010882e494b6866
karpenych <Dmitriy52444@gmail.com>
2023-02-19 13:34:41 +1000

    add_site_files

---
+++ stroki.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php
+    include "./inc/lib.inc";
+    $page = "stroki";
+    $z = "<h2>Работа с файлами и строками</h2>";
+?>
+<html>
+<head>
+    <title>Вывод файла задания</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+</head>
+<body>
+<?php
+    include "./inc/str.inc";
+?>
+</body>
+</html>
